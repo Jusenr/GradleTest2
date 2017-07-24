4f4341c4c60bc074a7ea93cbf3dba51cc9da6111
zhouxingrong <zhouxingrong@putao.com>
2017-07-24 17:46:50 +0800

    2222

--- app/src/main/java/com/jusenr/gradletest2/MainActivity.java
+++ app/src/main/java/com/jusenr/gradletest2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jusenr.gradletest2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("tag", BuildConfig.DOMAIN_NAME);
+
+
+        TextView tv_version_name = (TextView) findViewById(R.id.tv_version_name);
+        TextView tv_version_code = (TextView) findViewById(R.id.tv_version_code);
+
+        tv_version_name.setText(String.format("VersionName %s", BuildConfig.VERSION_NAME));
+        tv_version_code.setText(String.format("VersionName %d", BuildConfig.VERSION_CODE));
     }
 }
